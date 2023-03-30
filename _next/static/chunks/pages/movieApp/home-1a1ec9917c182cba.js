30e129f8d1e0765c697ee1cfd404129febd26225
darin.baek <eun.juuu0@gmail.com>
2023-03-30 23:19:10 +0900

    deploy to gh-pages

---
+++ _next/static/chunks/pages/movieApp/home-1a1ec9917c182cba.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[694],{3972:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/movieApp/home",function(){return u(999)}])}},function(n){n.O(0,[976,141,698,999,774,888,179],function(){return n(n.s=3972)}),_N_E=n.O()}]);
