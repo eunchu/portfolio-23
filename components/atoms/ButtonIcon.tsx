2e3f5ee4e45d10977dae8c62f29eea1aad7b4c99
darin.baek <eun.juuu0@gmail.com>
2023-04-08 01:52:02 +0900

    feat: 버튼 컴포넌트 구현 [atoms]

---
+++ components/atoms/ButtonIcon.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+const Button = styled.div<{ width: string }>`
+  display: flex;
+  align-items: center;
+
+  width: ${(props) => props.width};
+  height: 32px;
+
+  background-color: white;
+  text-shadow: none;
+  border-radius: 4px;
+
+  padding: 0 16px;
+  cursor: pointer;
+  &:hover {
+    background-color: #e6e6e6;
+  }
+  span {
+    font-size: 12px;
+    color: #000000;
+  }
+`;
+const Icon = styled.div`
+  color: #000000;
+  font-size: 14px;
+  margin-right: 8px;
+`;
+
+interface IButtonIconProps {
+  width?: string;
+  size?: "small" | "middle" | "large";
+  text: string;
+  icon: React.ReactNode;
+}
+const ButtonIcon = ({ width, size, text, icon }: IButtonIconProps) => {
+  return (
+    <Button width={width ?? "72px"}>
+      <Icon>{icon}</Icon>
+      <span>{text}</span>
+    </Button>
+  );
+};
+
+export default ButtonIcon;
