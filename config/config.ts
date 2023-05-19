5bfe163966e082472351c7c6f5613e038cd17bbd
darin.baek <eun.juuu0@gmail.com>
2023-05-20 01:29:13 +0900

    set: vercel 배포 삭제

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,4 @@
 export const prefix =
   process.env.NODE_ENV === "production"
-    ? "https://eunchu.github.io/portfolio-23"
+    ? "https://main--stupendous-frangipane-2563b4.netlify.app"
     : "";
